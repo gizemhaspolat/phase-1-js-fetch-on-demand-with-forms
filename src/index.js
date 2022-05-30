0317cf4fb3df705c6b0876386da061de65252b32
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-05-30 20:19:44 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //e.target.children[1].value;
+    const input = document.getElementById("searchByID");
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const movieTitle = document.querySelector("section#movieDetails h4");
+        const movieDescription = document.querySelector(
+          "section#movieDetails p"
+        );
+        movieTitle.innerText = data.title;
+        movieDescription.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
